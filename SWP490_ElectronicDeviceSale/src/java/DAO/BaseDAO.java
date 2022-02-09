b82ebf2e7e5eb4a13176cc03a10521c3e84c00d5
Trần Văn Cương <cuongvip104@gmail.com>
2022-02-09 20:29:26 +0700

    Code Base DAO

--- SWP490_ElectronicDeviceSale/src/java/DAO/BaseDAO.java
+++ SWP490_ElectronicDeviceSale/src/java/DAO/BaseDAO.java
@@ -4,10 +4,31 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ASUS
  */
 public class BaseDAO {
-    
+
+    protected Connection connection;
+
+    public BaseDAO() {
+        try {
+            String user = "se1514";
+            String pass = "12345678";
+            String url = "jdbc:sqlserver://ADMIN\\TRANCUONG:1433;databaseName=SWP391";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
