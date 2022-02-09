2a2556e3b0d4b7007369b08122ad2b78d242c9d6
Trần Văn Cương <cuongvip104@gmail.com>
2022-02-09 21:09:07 +0700

    Create package model

---
+++ SWP490_ElectronicDeviceSale/src/java/model/Product.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class Product {
+    private int id;
+    private Category c;
+    private Shop s;
+    private String name;
+    private String image;
+    private String description;
+    private int vote;
+    private float price;
+    private float discount;
+    private String status;
+    private Date created;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Category getC() {
+        return c;
+    }
+
+    public void setC(Category c) {
+        this.c = c;
+    }
+
+    public Shop getS() {
+        return s;
+    }
+
+    public void setS(Shop s) {
+        this.s = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+    
+}
---
+++ SWP490_ElectronicDeviceSale/src/java/model/Shop.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Shop {
+    private int id;
+    private String name;
+    private String address;
+    private String adminName;
+    private String usernameAdmin;
+    private String passwordAdmin;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getUsernameAdmin() {
+        return usernameAdmin;
+    }
+
+    public void setUsernameAdmin(String usernameAdmin) {
+        this.usernameAdmin = usernameAdmin;
+    }
+
+    public String getPasswordAdmin() {
+        return passwordAdmin;
+    }
+
+    public void setPasswordAdmin(String passwordAdmin) {
+        this.passwordAdmin = passwordAdmin;
+    }
+    
+}
