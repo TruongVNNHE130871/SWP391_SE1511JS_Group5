3ea86343bd9ed01d35a9efb1bc0ad9dba337086a
NguyenVinh98 <94851981+NguyenVinh98@users.noreply.github.com>
2022-02-10 03:08:57 +0700

    add user class

---
+++ SWP490_ElectronicDeviceSale/src/java/model/User.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author soide
+ */
+public class User {
+
+    private int id;
+    private String name;
+    private String userName;
+    private String passWord;
+    private boolean gender;
+    private int phone;
+    private String email;
+    private Date created;
+
+    public User() {
+    }
+
+    public User(int id, String name, String userName, String passWord, boolean gender, int phone, String email, Date created) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.passWord = passWord;
+        this.gender = gender;
+        this.phone = phone;
+        this.email = email;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+}
